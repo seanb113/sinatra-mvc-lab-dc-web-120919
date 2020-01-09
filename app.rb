c529e53da6fb779ddc450a58bbda265c9ab33faf
John Louis Rumingan <jmrumingan@gmail.com>
2020-01-09 14:03:26 -0500

    Added logic

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/'  do 
+
+        erb :user_input
+    end 
+
+    post '/piglatinize' do
+
+
+        @pig_phrase = Piglatinize.create(params[:phrase])
+        
+        erb :piglatinize
+    end 
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,27 @@
+class Piglatinizer
+
+    def piglatinize_phrase(phrase)
+
+    a = phrase.split(' ')
+    b = a.map {|word| piglatinize(word)}
+    b.join(' ')
+    end 
+  
+  def piglatinize(word)
+  
+    if ["a", "e", "i", "o", "u"].include?(word[0])
+  
+      return "#{word}way"
+    else 
+      word_array = word.split('')
+      loop do 
+          word_array << word_array.shift 
+  
+          break if word_array[0]=="a" || word_array[0] == "e" ||word_array[0]=="i" ||word_array[0]=="o" ||word_array[0]=="u"
+      end  
+    end 
+
+    return word_array.join('')+"ay"
+  end 
+
+end 
